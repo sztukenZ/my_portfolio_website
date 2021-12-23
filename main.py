e953df6a0c9aa1b4cb4d4777250ef91846316d5f
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2021-12-23 10:36:59 +0100

    applying contact form

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
+import email_validator
 
 app = Flask(__name__)
 
