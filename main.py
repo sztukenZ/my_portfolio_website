220141acfc0ba5368b536fe2644b7fc443673050
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2021-12-23 11:33:32 +0100

    Start work on contact form

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms import StringField, validators, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
 import email_validator
 
 app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
-@app.route("/")
+class ContactForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
+    message = TextAreaField(label='Message')
+    submit = SubmitField(label="Send message")
+
+
+@app.route("/", methods=['POST', 'GET'])
 def home():
-    return render_template("index.html")
+    cform = ContactForm()
+    if cform.validate_on_submit():
+        print(f"Name:{cform.name.data}, E-mail:{cform.email.data}, message:{cform.message.data}")
+    return render_template("index.html", form=cform)
 
 
 if __name__ == "__main__":
