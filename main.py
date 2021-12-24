70e43fd231f6fcb8b3283be42f4ff280882d440b
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2021-12-24 20:02:35 +0100

    Page finish

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, validators, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 from flask_bootstrap import Bootstrap
 import email_validator
+from flask_mail import Mail, Message
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+app.secret_key = os.environ.get('SECRET_KEY')
+
+app.config.update(dict(
+    MAIL_SERVER='smtp.googlemail.com',
+    MAIL_PORT=465,
+    MAIL_USE_TLS=False,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=os.environ.get('USERNAME'),
+    MAIL_PASSWORD=os.environ.get('PASSWORD')
+))
+
+mail = Mail(app)
 
 
 class ContactForm(FlaskForm):
@@ -22,10 +35,15 @@ class ContactForm(FlaskForm):
 def home():
     cform = ContactForm()
     if cform.validate_on_submit():
-        print(f"Name:{cform.name.data}, E-mail:{cform.email.data}, message:{cform.message.data}")
+        # print(f"Name:{cform.name.data}, E-mail:{cform.email.data}, message:{cform.message.data}")
+        msg = Message(f"Message from {cform.email.data}", sender=f"{os.environ.get('USERNAME')}@gmail.com",
+                      recipients=["krzysztof.jerzy.sztuk@gmail.com"])
+        msg.body = f"{cform.name.data}\n{cform.message.data}"
+        mail.send(msg)
+        flash('Your message has been sent')
+        return redirect(url_for('home'))
     return render_template("index.html", form=cform, now=datetime.utcnow())
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
